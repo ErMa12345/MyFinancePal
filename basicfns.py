4b21c2bf95621522f79222b80e42e40e736c1a09
Dhruv R. Adha <dhruv@lawn-128-61-73-24.lawn.gatech.edu>
2024-02-10 15:19:16 -0500

    Added documentation for basicfns

--- basicfns.py
+++ basicfns.py
@@ -6,14 +6,16 @@ import itertools
 from sklearn.linear_model import LinearRegression
 
 
+# Get average variance of a stock over the past year
 def getMeanVar(ticker):
   ibm_data = yf.Ticker(ticker)
-  hist_data_last_30_days = ibm_data.history(period="1y")
-  mean = hist_data_last_30_days['Close'].mean()
-  variance = hist_data_last_30_days['Close'].pct_change().std()
+  hist_data = ibm_data.history(period="1y")
+  mean = hist_data['Close'].mean()
+  variance = hist_data['Close'].pct_change().std()
   return (mean, variance)
 
 
+# Get standard deviation of stock returns for a certain ticker over its entire time period
 def getStdReturns(ticker):
   ibm_data = yf.Ticker(ticker)
   hist_data = ibm_data.history(period="max")
@@ -21,6 +23,7 @@ def getStdReturns(ticker):
   return volatility
 
 
+# Get the covariance between two stocks by analyzing correlation between their stock trends over the past year
 def covariance(ticker1, ticker2):
   ticker1_object = yf.Ticker(ticker1)
   ticker2_object = yf.Ticker(ticker2)
@@ -32,6 +35,7 @@ def covariance(ticker1, ticker2):
   return correlation_coefficient
 
 
+# Sum all of the covariances for each pair within a given portfolio
 def sumCovariances(portfolio):
   weighted_correlations = []
   ticker_objects = {ticker: yf.Ticker(ticker) for ticker in portfolio.keys()}
@@ -49,13 +53,15 @@ def sumCovariances(portfolio):
   return np.sum(weighted_correlations)
 
 
+# Get the portfolio variance by calling the sumCovariances function and adding
 def portfolioVariance(portfolio):
   var = sumCovariances(portfolio)
   for ticker, prop in portfolio.items():
-    var += (prop)**2 * getMeanVar(ticker)[1];
+    var += (prop)**2 * getMeanVar(ticker)[1]
   return var
 
 
+# Calculate the covariances between every pair and return the pairs from highest to lowest covariances
 def find_high_covariance_pairs(portfolio, threshold):
   high_covariance_pairs = []
   for pair in itertools.combinations(portfolio.keys(), 2):
@@ -68,6 +74,7 @@ def find_high_covariance_pairs(portfolio, threshold):
   return high_covariance_pairs
 
 
+# Get the beta value (amount of systematic risk relative to the S&P500) of a single stock
 def calculate_beta(ticker):
   asset_data = yf.Ticker(ticker).history(period="1y")['Close'].pct_change().dropna()
   market_index_data = yf.Ticker("VOO").history(period="1y")['Close'].pct_change().dropna()
@@ -77,6 +84,7 @@ def calculate_beta(ticker):
   return beta
 
 
+# Get the weighted betas using portfolio weights of each stock
 def weighted_betas(portfolio):
   portfolio_betas = {}
   for asset_ticker, weight in portfolio.items():
@@ -86,19 +94,19 @@ def weighted_betas(portfolio):
   return sorted_portfolio_betas
 
 
+# Get the beta of the entire portfolio (how risky is the portfolio relative to the P500)
 def calculate_portfolio_beta(portfolio):
   total_portfolio_beta = 0
   total_portfolio_weight = 0
-
   for asset_ticker, weight in portfolio.items():
     beta = calculate_beta(asset_ticker)
     total_portfolio_beta += beta * weight
     total_portfolio_weight += weight
-
   portfolio_beta = total_portfolio_beta / total_portfolio_weight
   return portfolio_beta
 
 
+# Return r squared of a stock compared to an index (percentage of a stock's movements that can be explained by movements in a benchmark index)
 def calculate_r_squared(asset_ticker, market_index_ticker):
   asset_data = yf.Ticker(asset_ticker).history(period="1y")['Close'].pct_change().dropna().values.reshape(-1, 1)
   market_data = yf.Ticker(market_index_ticker).history(period="1y")['Close'].pct_change().dropna().values.reshape(-1,
@@ -108,7 +116,7 @@ def calculate_r_squared(asset_ticker, market_index_ticker):
   r_squared = model.score(market_data, asset_data)
   return r_squared
 
-
+# Return r squared of a portfolio compared to an index (percentage of the portfolio's movements that can be explained by movements in a benchmark index)
 def calculate_portfolio_r_squared(portfolio, market_index_ticker, period="1y"):
   market_data = yf.Ticker(market_index_ticker).history(period=period)['Close'].pct_change().dropna().values.reshape(
     -1, 1)
@@ -126,6 +134,7 @@ def calculate_portfolio_r_squared(portfolio, market_index_ticker, period="1y"):
   return r_squared
 
 
+# Convert dictionary of tickers to number of shares to a dictionary of tickers to portfolio weights
 def generate_portfolio(shares_portfolio):
   stock_prices = {}
   for ticker, shares in shares_portfolio.items():
